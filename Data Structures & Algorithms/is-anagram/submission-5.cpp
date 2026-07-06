f11444de0232fb79f54ccbc94216f94efdcc1c0a
Hrishikesh Kali <kalihrishikesh@gmail.com>
2026-07-06 21:44:25 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()) return false;
+
+        map<char, int> freq;
+        for(auto x:s){
+            freq[x]++;
+        }
+        for(auto y:t){
+            freq[y]--;
+        }
+
+        for(auto z:freq){
+            if(z.second!=0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
